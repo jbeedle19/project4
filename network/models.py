5f38ff4c5d2c29175bd35e535808875d0a93e596
Joshua Beedle <josh.beedle@gmail.com>
2022-03-18 14:27:00 +0000

    outlined landing page with new post box and render of example post

--- network/models.py
+++ network/models.py
@@ -3,4 +3,25 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('self', blank=True, related_name='followers', symmetrical=False)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.CharField(max_length=300)
+    likes = models.PositiveIntegerField(default=0)
+    user_likes = models.ManyToManyField(User)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f'Post {self.id}: {self.user} said {self.content} on {self.created}'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
+    liked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user} liked {self.post}'
